4e0265310a0ec62e41e11cd2fe1a741ba385e9e0
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-05-18 11:23:46 +0100

    Basic handlebars templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,15 +3,37 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+
+// Express config paths
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, "../views");
 
-app.use(express.static(publicDirectoryPath))
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 
-app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 27,
-    location: 'Lodon'
-  })
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req,res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Andrei'
+  });
+});
+
+app.get('/about', (req,res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Andrei'
+  });
+});
+
+app.get('/help', (req,res) => {
+  res.render('help', {
+    title: 'Help Page',
+    message: 'No FAQ'
+  });
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
