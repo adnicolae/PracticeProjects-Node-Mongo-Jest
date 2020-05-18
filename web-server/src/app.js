33f90733fbf8988dcd4116a515259eb439a26389
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-05-18 12:48:33 +0100

    Add header and footer hbs partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const port = 3000;
 
 // Express config paths
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, "../views");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -32,7 +35,8 @@ app.get('/about', (req,res) => {
 app.get('/help', (req,res) => {
   res.render('help', {
     title: 'Help Page',
-    message: 'No FAQ'
+    message: 'No FAQ',
+    name: 'Andrei'
   });
 });
 
