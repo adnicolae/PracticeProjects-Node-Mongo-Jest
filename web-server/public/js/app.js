e79d9241ca72d52512fa9a6409c882c1a055bc47
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-05-19 10:27:21 +0100

    Setup app for heroku deployment

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -29,7 +29,7 @@ const clearInputs = () => {
 }
 
 const getWeather = location => 
-  fetch(`http://localhost:3000/weather?address=${ location }`)
+  fetch(`/weather?address=${ location }`)
   .then(res => res.json())
   .then(data => {
     if (data.error) {
