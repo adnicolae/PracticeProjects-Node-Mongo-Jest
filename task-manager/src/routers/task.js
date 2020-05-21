9954313a89ad7a625176ac5001382478c8fcd76d
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-05-21 07:45:09 +0100

    Refactor using routes

---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = new express.Router();
+const Task = require('../models/task');
+
+router.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
+
+  Task.find({}).then((tasks) => {
+    res.send(tasks);
+  }).catch((e) => {
+    res.status(500).send();
+  });
+});
+
+router.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  };
+});
+
+router.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(e);
+  }
+});
+
+router.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidUpdate = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) {
+    return res.status(400).send({ error: 'Not a valid operation.' });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, 
+      { new: true, runValidators: true });
+    
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
+router.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(400);
+    }
+    
+    res.send(task);
+  } catch (error) {
+    res.status(400).send();
+  }
+})
+
+module.exports = router;
