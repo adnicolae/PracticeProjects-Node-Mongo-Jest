cdc2665c12cd8d5ae1fe6c3b546a703038726445
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-05-26 07:57:55 +0100

    Add timestamps, filtering, paginating and sorting

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -17,20 +17,37 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true/false
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt_desc
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+  }
+
   try {
     // const tasks = await Task.find({ owner: req.user._id });
-    await req.user.populate('tasks').execPopulate();
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate();
     res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send();
   }
-
-  Task.find({}).then((tasks) => {
-    res.send(tasks);
-  }).catch((e) => {
-    res.status(500).send();
-  });
 });
 
 router.get('/tasks/:id', auth, async (req, res) => {
